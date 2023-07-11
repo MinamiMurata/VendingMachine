0080501afdd3771c149da229f49dce90916bcb06
yuki kondo <jinteng.yougui@gmail.com>
2023-07-11 16:42:08 +0900

    created choice5

--- vm.rb
+++ vm.rb
@@ -12,14 +12,14 @@ class VendingMachine
   end
 
   # 投入金額の総計を取得できる。
-  def current_slot_money
-    # 自動販売機に入っているお金を表示する
-    @@slot_money
-  end
+  # def current_slot_money
+  #   # 自動販売機に入っているお金を表示する
+  #   @@slot_money
+  # end
 
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   # 投入は複数回できる。
-  def slot_money(money)
+  def slot_money
     puts "お金ちょうだい"
     money = gets.chomp.to_i
     # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
@@ -27,14 +27,14 @@ class VendingMachine
     return false unless MONEY.include?(money)
     # 自動販売機にお金を入れる
     @@slot_money += money
-    puts "投入金額：#{@@slot_money}円"
+    puts "投入金額：#{money}円  投入金額合計:#{@@slot_money}円"
     # @@slot_money += money この行いらない気がしたのでコメントアウト
   end
 
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
   def return_money
     # 返すお金の金額を表示する
-    puts @@slot_money
+    puts "お釣り : #{@@slot_money}円"
     # 自動販売機に入っているお金を0円に戻す
     @@slot_money = 0
   end
@@ -42,45 +42,23 @@ end
 
 # ジュースの管理
 class Stock < VendingMachine
-  # コーラの値段は1本120円
-  # DEFAULT_NAME = "cola"
-  # DEFAULT_PRICE = 120
-  # # 初期状態でコーラを5本格納している
-  # DEFAULT_NUMBER = 5
   # #読み取り用のメソッドを定義
   # attr_accessor :name, :price, :number
 
-  # def initialize(name = DEFAULT_NAME, price = DEFAULT_PRICE, number = DEFAULT_NUMBER)
-    # @name = name
-    # @price = price
-    # @number = number
-  # end
   def initialize
-    @cola = {name: "コーラ", price: 120, stock: 5 } #追記
-    @water = {name: "水", price: 100, stock: 5 } #追記
-    @red_bull = {name: "レッドブル",price: 200, stock: 5 } #追記
+    @cola = { name: "コーラ", price: 120, stock: 5 }
+    @water = { name: "水", price: 100, stock: 5 }
+    @red_bull = { name: "レッドブル", price: 200, stock: 5 }
     # 自販機に入っているドリンク3種類の情報を配列に格納
-    @drinks = [@cola, @water, @red_bull] #追記
+    @drinks = [@cola, @water, @red_bull]
   end
 
   # 値段と名前と在庫を取得できる
-  # def current_drink
-  #   puts "#{@name}は#{@price}円で在庫は#{@number}本です。"
-  # end
   def inform_drink_types
     puts "#{@drinks[0][:name]}は#{@drinks[0][:price]}円で在庫は#{@drinks[0][:stock]}本です。"
     puts "#{@drinks[1][:name]}は#{@drinks[1][:price]}円で在庫は#{@drinks[1][:stock]}本です。"
     puts "#{@drinks[2][:name]}は#{@drinks[2][:price]}円で在庫は#{@drinks[2][:stock]}本です。"
   end
-
-  # drinks = []
-  # cola = { name: "コーラ", price: 120, number: 5 }
-  # redbull = { name: "レットブル", price: 200, number: 5 }
-  # water = { name: "水", price: 100, number: 5 }
-
-  # drinks << cola
-  # drinks << redbull
-  # drinks << water
 
   def drink_list
     slot_money = @@slot_money
@@ -91,36 +69,68 @@ class Stock < VendingMachine
     elsif slot_money >= 100
       puts "現在#{@drinks[1][:name]}が購入可能です。"
     else
-      puts "購入不可能です！！お金ください♡"
+      puts "購入不可能です！！お金ちょうだい♡"
     end
   end
 end
 
 class Sales < Stock
   def initialize
+    super
     @sales = 0
   end
 
-  def purchase(money)
-    if @@slot_money > @price
-      @@slot_money -= @price
-      @number -= 1
-      @sales += @price
-      return_money
+  def purchase
+    puts "何を買いますか？"
+    puts "1.コーラ 2.水 3.レッドブル"
+    choice_drink = gets.to_i - 1
+    if make_sure_enough_money_to_buy_(choice_drink)
+      purchase_process(choice_drink)
+      puts "ガチャン"
+      puts "#{@drinks[choice_drink][:name]}"
+    else
+      drink_list
     end
+  end
+
+  def make_sure_enough_money_to_buy_(drink)
+    @@slot_money > @drinks[drink][:price]
+  end
+
+  def purchase_process(drink)
+    @@slot_money -= @drinks[drink][:price]
+    @drinks[drink][:stock] -= 1
+    @sales += @drinks[drink][:price]
   end
 
   def current_sales
     puts "現在の売上金額は#{@sales}です。"
   end
 
-  def stocks
-    puts "現在#{@name}は残り#{@number}本です。"
-  end
+  # def stocks
+  #   puts "現在#{@name}は残り#{@number}本です。"
+  # end
 end
 
 vm = VendingMachine.new
-vm.slot_money(0)
 stock = Stock.new
-stock.drink_list
+sales = Sales.new
 
+while true
+  puts "---------------------\nどうする？\n---------------------\n1.在庫を見る\n2.お金を入れる\n3.購入可能な商品を見る\n4.商品を購入する\n5.やめる\n---------------------"
+  choice = gets.chomp.to_i
+  if choice == 1
+    sales.inform_drink_types
+  elsif choice == 2
+    vm.slot_money
+  elsif choice == 3
+    stock.drink_list
+  elsif choice == 4
+    sales.purchase
+  elsif choice == 5
+    vm.return_money
+    return
+  else
+    puts "1~4の数字を入力してください。"
+  end
+end
